cba6944c417cf48f5b8b846da6c6b6fea0af27b9
Ismail Silat <ismailsilat7@gmail.com>
2025-07-16 23:01:53 +0500

    Add event listeners for formContent

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import ToDoManager from "./ToDoManager";
 import "./style.css"
-import { 
-    priorityRank,
-    isToday,
-    isTomorrow,
-    isThisWeek,
-    parseISO,
-    format,
-    isBefore,
-    differenceInDays,
-    isSameDay,
-    isAfter
-} from "./ToDoManager";
-
+import { updateProject } from "./form/from";
 import taskContent from "./filter/filter";
-
-import hashIconRaw from "./images/hash.svg?raw";
-import deleteIcon from "./images/delete.svg";
-
+import formContent from "./form/from";
 import logoIcon from "./images/logo-nobg-white.png";
 import sidebarIcon from "./images/sidebar-icon.png";
 import addIcon from "./images/add.svg";
@@ -29,67 +14,8 @@ import overdueIcon from "./images/overdue.svg";
 import completedIcon from "./images/completed.svg";
 import addProjectIcon from "./images/addproject.svg"
 
-
-function injectColoredIcon(color) {
-    if(color == null) {
-        color = "#ffffff";
-    }
-    const wrapper = document.createElement("div");
-    wrapper.innerHTML = hashIconRaw;
-    const svg = wrapper.querySelector("svg");
-
-    svg.style.width = "16px";
-    svg.style.height = "16px";
-    svg.style.fill = color;
-
-    return svg;
-}
-
-
 const manager = new ToDoManager();
 window.manager = manager;
-
-const projects = document.querySelector(".projectsButtons");
-
-function updateProject() {
-    projects.innerHTML = "";
-    for (let i = 0; i < manager.projects.length; i++) {
-    
-        const project = manager.projects[i];
-        const div = document.createElement("div");
-        div.id = project.id;
-        div.classList.add("project-entry");
-    
-        const name = document.createElement("span");
-        name.classList.add("viewButton");
-
-        const colorSpan = document.createElement("span");
-        const svg = injectColoredIcon(project.color);
-        colorSpan.appendChild(svg);
-        
-        const nameSpan = document.createElement("span");
-        nameSpan.textContent = project.title.toLowerCase();
-        name.append(colorSpan, nameSpan);
-
-        const removeBtn = document.createElement("button");
-        removeBtn.classList.add("removeBtn")
-        const removeImg = document.createElement("img");
-        removeImg.src = deleteIcon;
-        removeBtn.append(removeImg);
-        removeBtn.onclick = () => {
-            manager.removeProject(project.id);
-            div.remove();
-        };
-    
-        div.append(name, removeBtn);
-        projects.appendChild(div);
-
-        name.addEventListener('click', () => {
-            const projectTasks = manager.filterTasks("project", project.id);
-            taskContent(document.querySelector("#content"), project.title, projectTasks, manager, true);
-        });
-    }
-}
 
 const iconMap = {
   logo: logoIcon,
@@ -104,16 +30,20 @@ const iconMap = {
   addproject: addProjectIcon,
 };
 
+// had to convert this to function as previously the load time data was being storeddd :/
 const views = {
-    inbox: manager.filterTasks("inbox"),
-    today: manager.filterTasks("today"),
-    thisWeek: manager.filterTasks("thisWeek"),
-    upcoming: manager.filterTasks("upcoming"),
-    overdue: manager.filterTasks("overdue"),
-    completed: manager.filterTasks("completed"),
+    inbox: () => manager.filterTasks("inbox"),
+    today: () => manager.filterTasks("today"),
+    thisWeek: () => manager.filterTasks("thisWeek"),
+    upcoming: () => manager.filterTasks("upcoming"),
+    overdue: () => manager.filterTasks("overdue"),
+    completed: () => manager.filterTasks("completed"),
 };
 
 
+function removeActiveSidebar() {
+    document.querySelectorAll(".viewButton").forEach(b => b.classList.remove("active-sidebar"));
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll("img[data-icon]").forEach((img) => {
@@ -126,36 +56,50 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelectorAll(".button").forEach(button => {
         button.addEventListener('click', (e) => {
-            taskContent(document.querySelector("#content"), button.id, views[button.id], manager);
+            taskContent(document.querySelector("#content"), button.id, views[button.id](), manager);
         });
     });
 
     document.querySelectorAll(".viewButton").forEach(btn => {
         btn.addEventListener('click', (e) => {
-            document.querySelectorAll(".viewButton").forEach(b => b.classList.remove("active-sidebar"));
+            removeActiveSidebar();
             btn.classList.add("active-sidebar");
         })
     })
-
-    document.querySelector("#addTask").addEventListener("click", (e, b) => {
-        document.querySelectorAll(".viewButton").forEach(b => b.classList.remove("active-sidebar"));
+    const addTask = document.querySelector("#addTask")
+    addTask.addEventListener("click", () => {
+        removeActiveSidebar();
         document.querySelector("#content").textContent = ""
-        b.classList.add("active-sidebar");
+        formContent("addTask")
     });
 
-    document.querySelector("#addProject").addEventListener("click", (e, b) => {
-        document.querySelectorAll(".viewButton").forEach(b => b.classList.remove("active-sidebar"));
+    const addProject = document.querySelector("#addProject")
+    addProject.addEventListener("click", () => {
+        removeActiveSidebar();
         document.querySelector("#content").textContent = ""
-        b.classList.add("active-sidebar");
+        addProject.classList.add("active-sidebar");
+        formContent("addProject")
     });
+
+    if(window.innerWidth < 768) {
+        document.querySelector(".sidebar-content").classList.toggle("inactive-sidebar");
+    }
 
 });
 
-taskContent(document.querySelector("#content"), "inbox", views["inbox"], manager);
+taskContent(document.querySelector("#content"), "inbox", views["inbox"](), manager);
 document.querySelector("#inbox").classList.add("active-sidebar");
 
 const sidebar = document.querySelector(".sidebar-content");
 document.querySelector("#sidebar-icon").addEventListener('click', () => {
     sidebar.classList.toggle("inactive-sidebar");
 })
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+        if (sidebar.classList.contains("inactive-sidebar")) {
+            sidebar.classList.remove("inactive-sidebar");
+        }
+    }
+});
 
